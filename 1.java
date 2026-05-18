0bce3b1eccd757335c6ab3cefddb31f4b6541a57
Naitoshi-lang <dmitryshadrin7@gmail.com>
2026-05-18 15:12:42 +0300

    Create 1.java

---
+++ 1.java
@@ -0,0 +1,12 @@
+let numbers = [1, 2, 3];
+
+// Добавь в конец 4 и 5
+numbers.push(4, 5);
+console.log(numbers); // [1, 2, 3, 4, 5]
+
+// Удали последний элемент
+numbers.pop();
+console.log(numbers); // [1, 2, 3, 4]
+
+// Итоговый массив
+console.log("Итоговый массив:", numbers); // Итоговый массив: [1, 2, 3, 4]
