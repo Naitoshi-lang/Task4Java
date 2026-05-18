1992f481898f81eb9c0a182a417978c2447734fa
Naitoshi-lang <dmitryshadrin7@gmail.com>
2026-05-18 15:12:59 +0300

    Create 2.java

---
+++ 2.java
@@ -0,0 +1,9 @@
+let inventory = ["sword", "shield", "potion"];
+
+// Проверь, есть ли "potion"
+let hasPotion = inventory.includes("potion");
+console.log("Есть ли potion?", hasPotion); // true
+
+// Найди индекс "shield"
+let indexShield = inventory.indexOf("shield");
+console.log("Индекс shield:", indexShield); // 1
